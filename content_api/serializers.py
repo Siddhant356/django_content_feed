66c876fde86bc558356264095ffaa88ae682a8c4
Siddhant356 <sidhantshukla356@gmail.com>
2020-03-30 15:12:20 +0530

    JASON api using restframework Viewsets

---
+++ content_api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from content_api.models import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = (
+            'id',
+            'title',
+            'description'
+        )
